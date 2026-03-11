c5d6289af087637a6df04c211650cb5877009e5b
Josh <59983151+jhumms@users.noreply.github.com>
2026-03-11 20:52:24 +0530

    Added in new views via models

---
+++ airbnb/models/src/src_listings.sql
@@ -0,0 +1,15 @@
+With raw_listings as (
+select * from AIRBNB.RAW.RAW_LISTINGS
+)
+
+SELECT 
+   id AS listing_id,
+   listing_url,
+   name AS listing_name,
+   room_type,
+   minimum_nights,
+   host_id,
+   price AS price_str,
+   created_at,
+   updated_at
+FROM raw_listings
